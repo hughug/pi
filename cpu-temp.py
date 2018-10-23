331ad4a1c2ddc7d819bed2da3867570a8de9f754
hughug <cbywyy@gmail.com>
2018-10-22 23:24:07 -0700

    add program to monitor cpu temperature

---
+++ cpu-temp.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# this program reads the cpu temperature
+
+import io 
+
+def gettemp():
+   f = open("/sys/class/thermal/thermal_zone0/temp", "r") 
+   t = f.readline ()
+   CelsiusTemp = float(t)/1000
+   return CelsiusTemp
+
+cputemp = "CPU temp: "+'%.1f'%gettemp()+'°C'
+print(cputemp)
